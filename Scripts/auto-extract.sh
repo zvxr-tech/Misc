16105283236595fe9603a9187119ed3174ba0471
Mike Clark <zvxr-tech@users.noreply.github.com>
2016-08-08 08:10:57 -0600

    initial commit of auto-extract script skeleton.

---
+++ Scripts/auto-extract.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Auto-extractor
+
+# Check for and enumerate new downloads
+
+# Determine the type of video (Movie/Television)
+
+# Find the video
+
+# Find any subtitles
+
+# Create destination
+
+# Extract to destination
+
+exit 0
